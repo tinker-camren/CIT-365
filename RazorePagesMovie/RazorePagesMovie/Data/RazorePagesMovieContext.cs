879db636de5f62b61be9ec282b61f11ef6832ae7
Camren Tinker <tinkerc@byui.edu>
2019-10-26 17:01:06 -0600

    Inital tutorial stuff for Week 06

---
+++ RazorePagesMovie/RazorePagesMovie/Data/RazorePagesMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorePagesMovie.Models
+{
+    public class RazorePagesMovieContext : DbContext
+    {
+        public RazorePagesMovieContext (DbContextOptions<RazorePagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorePagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorePagesMovie/RazorePagesMovie/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorePagesMovie.Models;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace RazorePagesMovie
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.
+                        GetRequiredService<RazorePagesMovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
